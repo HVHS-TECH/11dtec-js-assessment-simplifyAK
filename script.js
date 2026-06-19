df0ba2a6767a107f98320abc566b0d8ad7c09cf2
simplifyAK <24129ak@hvhs.school.nz>
2026-06-19 14:53:49 +1200

    Adding content

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
 console.log("Hello world!")
+
+
+
+const OUTPUT = document.getElementById("JavaScriptOutput")
+const NAME_FIELD = document.getElementById("nameField");
+const MONEY_FIELD = document.getElementById("moneyField");
+
+
+
+
+
+function getNameInput() {
+    let userName = NAME_FIELD.value;
+    OUTPUT.innerHTML += "<br>Your name is "+ userName+""
+}
+
+function calculateChange(_money, _price) {
+    let change = _money - _price;
+    return change;
+}
+
+function getMoneyInput() {
+    let userMoney = Number(MONEY_FIELD.value);
+
+    OUTPUT.innerHTML += "<p>You have " + userMoney + " dollars";
+
+    if (userMoney >= 4) {
+        OUTPUT.innerHTML += "<br>You have enough money<br>";
+
+        let change = calculateChange(userMoney, 4);
+
+        OUTPUT.innerHTML += "<br>You will get $" + change + " of change";
+    } else {
+        OUTPUT.innerHTML += "<p>You litreally can't even afford a chocolate bar</p>";
+    }
+
+    if (MONEY_FIELD.value <= 0) {
+        OUTPUT.innerHTML = "<br>You can't really have 0$ right??? Lock in and make some money";
+    } else if (MONEY_FIELD.value > 9999999999999) {
+        OUTPUT.innerHTML = "<br> You have more money that trees in the world. This is cap"
+    } else if (MONEY_FIELD.checkValidity() === false){
+        OUTPUT.innerHTML = "<br>Please fill in the form correctly."
+    } else    {
+        // Process the form normally
+    }
+   
+}
+let foodArray = ["Cake", "Crepes", "Feastables", "SweetIcedTea", "Milkshake"]
+
+
+
+
+
