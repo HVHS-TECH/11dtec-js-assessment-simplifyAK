e4180b272482a8da60a047dcae22bcc11e37c1c2
simplifyAK <24129ak@hvhs.school.nz>
2026-07-24 15:11:37 +1200

    Changed different css code

--- order.js
+++ order.js
@@ -1,13 +1,19 @@
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
+//* Get saved cart data or starts with an empty array / list *//
 
+
+
+//* lets and consts *//
 let OUTPUT = document.getElementById("JavaScriptOutput");
-
 const NAME_FIELD = document.getElementById("nameField");
 const MONEY_FIELD = document.getElementById("moneyField");
 
 let total = 0;
+//* Array of menu items on the order page *//
 let menuItems = ['Cake - $7.50', 'Crepes - $12.50', 'Milkshake - $8.50', 'SweetIcedTea - $7.50', 'Feastables - $6.50'];
 
+
+//* Displays the array *//
 for (let item of menuItems) {
     OUTPUT.innerHTML += item + "<br>";
 }
@@ -17,9 +23,10 @@ for (let item of menuItems) {
 
 if (cart.length === 0) { 
     OUTPUT.innerHTML += "<h2>Your Cart is Empty</h2>";
+    //* (Shows a message that if the user has an empty cart that its empty) *//
 } else {
     OUTPUT.innerHTML += "<h2>Your Cart</h2>";
-
+    //* Shows carts items and calculate a total *//
     for (let item of cart) {
         OUTPUT.innerHTML += item.name + " - $" + item.price.toFixed(2) + "<br>";
         total += item.price;
@@ -36,27 +43,28 @@ if (cart.length === 0) {
 function getNameInput() {
     let userName = NAME_FIELD.value;
     if (userName === "") {
-     OUTPUT.innerHTML += "<p>Please enter your name</p>";
+     alert("Please enter your name");
     }
     OUTPUT.innerHTML += "<br>Your name is "+ userName+""
 }
 
 
 function getMoneyInput() {
-
+    //* Stops the action if there is no items in the cart *//
    if (cart.length === 0) {
-    OUTPUT.innerHTML += "<p>Your Cart is Empty</p>";
-    return;
+    alert("Please enter items into the cart");
    }
 
     let userMoney = Number(MONEY_FIELD.value);
-
+    //* Lets the user know if they dont have enough money *//
     if (userMoney < total) {
         let amountNeeded = total - userMoney;
         OUTPUT.innerHTML += "<p>Sorry, you don't have enough money.</p>";
         OUTPUT.innerHTML += "<p>You need $" + (amountNeeded.toFixed(2)) + " to pay for your order</p>";
         return;
     } else {
+
+    //* Change after user payment *//
         let change = userMoney - total;
 
         OUTPUT.innerHTML += "<p>You paid $" + userMoney.toFixed(2) + "</p>";
